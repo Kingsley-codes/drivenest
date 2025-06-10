3abe786879dbfa8eae97edbccbd1759f161f1612
Kingsley-codes <agbamkingsley@gmail.com>
2025-06-10 17:16:37 +0100

    sth

--- server/api/utils/email.js
+++ server/api/utils/email.js
@@ -6,8 +6,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ to, subject, html }) => {
     try {
+
+        console.log('Attempting to send email to:', to);
         const response = await resend.emails.send({
-            from: 'https://drivenest-se33.onrender.com', // Dev sender
+            from: 'onboarding@resend.dev', // Dev sender
             to,
             subject,
             html,
